8e11b0b0a00ec2877b19e0366db00d39b333c9da
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-01 13:11:07 +0000

    HTML updated for basic search.  Some JS added for search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,29 @@
 
 const apiKey = "ddcaa7c9ad8c545d308663b61cde1942";
 
-
 const units = "metric"; //can use a selector no units = Kelvin, &units=metric = centigrade, &units=imperial = fahrenheit
 const apiCallType = "weather"; //Current weather, Change to "forecast" for 5 day forecast
 
-const city = document.getElementById("location");
+const apiUrl = `https://api.openweathermap.org/data/2.5/${apiCallType}?q=${searchCity}&appid=${apiKey}&units=${units}`;
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/${apiCallType}?q=Bristol&appid=${apiKey}&units=${units}`;
+const searchCity = document.getElementById("location");
+const buttonSearch = document.getElementById("get-weather");
+buttonSearch.addEventListener('click', handleClick);
+
+const temperature = document.getElementById("temp");
+const weather = document.getElementById("outlook");
+
+// fetch(apiUrl)                        //Connection Testing
+//     .then(function (response) {
+//         return response.json();
+//     }).then(function (object) {
+//         console.log(object);
+//     });
 
 
-fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    }).then(function (object) {
-        console.log(object);
-    });
-    
+function handleClick() {
+    const cityInput = searchCity.value;
+    //error /error catch for searchCity.innerText = "";
+
+
+}
