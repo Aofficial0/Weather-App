2a71f21d84fbfd5d1717bd9a65e9835ac9dcde08
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-01 11:02:17 +0000

    API connection test installed.  Background test images added.  CSS updated to refer to background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 "use strict";
 
+const apiKey = "ddcaa7c9ad8c545d308663b61cde1942";
 
-1+2
+
+const units = "metric"; //can use a selector no units = Kelvin, &units=metric = centigrade, &units=imperial = fahrenheit
+const apiCallType = "weather"; //Current weather
+
+const city = document.getElementById("location");
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/${apiCallType}?q=Bristol&appid=${apiKey}&units=${units}`;
+
+
+fetch(apiUrl)
+    .then(function (response) {
+        console.log(json);
+    }).then(function (object) {
+        console.log(object);
+    });
+    
